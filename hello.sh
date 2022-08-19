db6a8df101c8c0c512a50289b61d818556341a63
RaheezBZ <109944175+RaheezBZ@users.noreply.github.com>
2022-08-19 20:44:04 +0530

    Simple Startoff Script

---
+++ hello.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+echo "Hello World"
+
+#name of bash shell
+echo $BASH
+#System Variables
+echo $HOME
+echo $PWD
+echo $LS
+echo $BASH_VERSION
+name="Mohammed Raheez"
+echo Hello This is $name
+
